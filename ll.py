1f649aef78202a9e601d4858609ccd51c237845b
sharan <sharangowda2610@gmail.com>
2024-09-21 22:21:13 +0530

    pop first done

--- ll.py
+++ ll.py
@@ -37,6 +37,10 @@ class LinkedList:
         self.length += 1
 
     def pop(self):
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            return None
         temp = self.head
         pre = self.head
         while temp.next:
@@ -47,7 +51,27 @@ class LinkedList:
         self.length -= 1
 
     def pop_first(self):
-        pass
+        if self.length == 1:
+            return None
+        pre = self.head
+        next = self.head.next
+        self.head = next
+        pre.next = None
+        self.length -= 1
+        if self.length == 1:
+            return self.tail
+
+    def get(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.head
+        if index == self.length:
+            return self.tail
+        if 0 < index < self.length:
+            temp = self.head
+            while temp:
+                pass
 
 
 new_ll = LinkedList(4)
@@ -56,7 +80,9 @@ new_ll.append(6)
 new_ll.append(7)
 new_ll.prepend(3)
 new_ll.prepend(2)
+new_ll.prepend(1)
 new_ll.pop()
+new_ll.pop_first()
 new_ll.print_value()
 
 print("-------------------------------------------------------------")
